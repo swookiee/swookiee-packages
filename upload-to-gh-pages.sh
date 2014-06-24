3dcae48a6b6cdbe0b832fc778d6b68083fb1a55a
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-06-24 13:03:07 +0200

    a little cleanup

--- upload-to-gh-pages.sh
+++ upload-to-gh-pages.sh
@@ -7,15 +7,17 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #go to home and setup git
   cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
+  git config --global user.email "lars@mobilecologne.de"
+  git config --global user.name "Lars Pfannenschmidt"
 
   #using token clone gh-pages branch
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/swookiee/packages.git  gh-pages > /dev/null
 
-  #go into dir and copy data we're interested in to that directory
+  #go into dir and copy data we're interested in
   cd gh-pages
+  rm -rf *.rpm
   cp -Rf $HOME/uploads/* .
+  # will be updated when we have a release version and we use tags
   ln -sf swookiee-1.0.0_SNAPSHOT-$TRAVIS_BUILD_NUMBER.noarch.rpm swookiee-1.0.0_SNAPSHOT-latest.noarch.rpm
 
   #add, commit and push files
