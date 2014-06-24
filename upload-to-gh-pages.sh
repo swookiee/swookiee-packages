4750f909590777b1ea0121c3c2f16c1586de05d8
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-06-24 12:03:34 +0200

    Playing with gh pages upload

---
+++ upload-to-gh-pages.sh
@@ -0,0 +1,25 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to upload gh-pages\n"
+
+  #copy rpms
+  cp -R target/*.rpm $HOME/uploads
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/swookiee/packages.git  gh-pages > /dev/null
+
+  #go into dir and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/uploads/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Just uploaded latest build rpm artefacts\n"
+fi
